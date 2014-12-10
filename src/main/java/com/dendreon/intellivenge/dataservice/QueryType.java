ca858f1524b659718f16242ea9a06ce7db86fbd0
tracyhires <thires@teknovare.com>
2014-12-10 14:26:42 -0700

    added extra query types

--- src/main/java/com/dendreon/intellivenge/dataservice/QueryType.java
+++ src/main/java/com/dendreon/intellivenge/dataservice/QueryType.java
@@ -1,5 +1,5 @@
 package com.dendreon.intellivenge.dataservice;
 
 public enum QueryType {
-	EQ, GT, GTE, LT, LTE, CONTAINS, NEQ;
+	EQ, GT, GTE, LT, LTE, CONTAINS, NEQ, IN, INNER_JOIN, OUTER_JOIN;
 }
